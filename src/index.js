8f0df541bd3f36cb4f93e21fc2a871dec97e1fc7
Vlad_Ageysha <ander123fakk@gmail.com>
2020-11-19 19:02:23 +0300

    create component - App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App/App';
 
 ReactDOM.render(
-    <div>Hello</div>,
+    <App />,
     document.getElementById('root')
 );
