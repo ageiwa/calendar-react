5bfb039ec72c9f407a006fbbd2e9cf50a7caa80f
vlad ageysha <ander123fakk@gmail.com>
2021-01-05 14:15:09 +0300

    made a selection of the current day's color

--- src/App/components/SlideDays/SlideDays.js
+++ src/App/components/SlideDays/SlideDays.js
@@ -15,15 +15,14 @@ class SlideDays extends React.Component {
     }
 
     loadNum() {
-        const gDT = new Date();
-        const lDT = new Date(gDT.getFullYear(), gDT.getMonth(), 1);
+        const dt = new Date();
+        const lDT = new Date(dt.getFullYear(), dt.getMonth(), 1);
 
         let number = 1,
             firstDay = lDT.getDay() - 1 === -1 ? 6 : lDT.getDay() - 1;
 
         for (let i = 0; i < 42; i++) {
             const lDTnextMonth = new Date(lDT.getFullYear(), lDT.getMonth(), number);
-
             if (lDT.getMonth() !== lDTnextMonth.getMonth()) break;
 
             const newCurMonth = this.state.curMonth;
@@ -43,7 +42,10 @@ class SlideDays extends React.Component {
             <div className="slide-days">
                 <div className="container-days" >{
                     this.state.curMonth.map((num, i) => {
-                        return <div className="num" key={i}>{num}</div>;
+                        const dt = new Date();
+
+                        if (dt.getDate() === num) return <div className="num cur-num" key={i}>{num}</div>;
+                        else return <div className="num" key={i}>{num}</div>;
                     })
                 }</div>
             </div>
